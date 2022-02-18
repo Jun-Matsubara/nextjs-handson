c17dffeb8deb92ab1888741459bfd5e07e21269e
jun <19db115@okiu.ac.jp>
2022-02-18 12:02:51 +0900

    add HelloWord

---
+++ pages/part1/HelloWord.js
@@ -0,0 +1,11 @@
+import React from 'react'; //必ず書く文章(htmlを使うなら)
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <div>Welcome to Next.js!</div>
+    </div>
+  );
+};
+
+export default HelloWorld;
