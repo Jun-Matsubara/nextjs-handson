29bf2fc90064452e051751c8f96055a2be901288
任意のユーザ名 <>
2022-02-18 14:17:02 +0900

    add theme.js

---
+++ styles/theme.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { createTheme } from '@mui/material/styles';
+import { green, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: green,
+    secondary: purple,
+  },
+});
+
+export default theme;
