da4a1324ce5edb9b3bcbd3035a596703f41a37d9
任意のユーザ名 <>
2022-02-20 14:43:27 +0900

    コメントの追加

--- pages/api/shops.js
+++ pages/api/shops.js
@@ -5,9 +5,13 @@ const shops = async (req, res) => {
 
   const query = new URLSearchParams();
   query.set('key', HOTPEPPER_API_KEY);
+  //APIキーのセット
   query.set('format', 'json');
+  //様式はjsonで
   query.set('large_area', req.query.large_area || 'Z098');
+  //地域は指定があれば指定されたやつ、なければ沖縄（Z098)で
   if (req.query.keyword) query.set('keyword', req.query.keyword);
+  //もし、キーワードがあるならキーワードを追加して検索する
 
   const response = await fetch(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?${query.toString()}`);
   const data = await response.json();
